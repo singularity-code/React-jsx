3c8f85ceffcf20fefa94dd646b49f0f706feaba9
Chris <neoxrock@gmail.com>
2019-06-14 17:15:10 +1000

    HTML For Syntax

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const App = () => {
 
   return (
     <div>
-      <label className="label" for="name">
+      <label className="label" htmlFor="name">
         Enter name:
       </label>
       <input id="name" tyep="text" />
